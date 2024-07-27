87c7ca5655cd0d0acaff5524cd27684b07089227
samatha <samathagamidi@gmail.com>
2024-07-27 18:03:43 +0200

    Cleaned wrong data 'Summe' and changed 'MONAT' data format

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -13,5 +13,10 @@ traffic_data['WERT']=traffic_data['WERT'].fillna(0)
 print("Null values after handling:")
 print(traffic_data.isnull().any())
 
+# Drop the rows which contains the Summe in 'JAHR' 
+traffic_data=traffic_data[traffic_data['MONAT']!='Summe']
+# Convert 'MONAT' column data  from YYYYMM to MM format
+traffic_data['MONAT']=traffic_data['MONAT'].astype(str).str[4:6].astype(int)
+
 # Save preprocessed data
 traffic_data.to_csv('data/processed_data.csv',index=False)
